14593729a8cd54b83fdb20d90309621dd3d8fb6d
MyViboon <95564348+MyViboon@users.noreply.github.com>
2023-03-16 19:31:33 +0700

    Add Slice

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,16 +6,33 @@ const Navbar = () => {
     <div className={styles.container}>
       <div className={styles.item}>
         <div className={styles.callButton}>
-          <Image src="/img/telephone.png" alt="" width="32" height="32"/>
+          <Image src="/img/telephone.png" alt="" width="32" height="32" />
         </div>
         <div className={styles.texts}>
-          <div className={styles.text}>ORDER NOW</div>
+          <div className={styles.text}>ORDER NOW!</div>
           <div className={styles.text}>095-165-8365</div>
         </div>
       </div>
 
-      <div className={styles.item}>Center</div>
-      <div className={styles.item}>right</div>
+      <div className={styles.item}>
+        <ul className={styles.list}>
+          <li className={styles.listItem}>Homepage</li>
+          <li className={styles.listItem}>Products</li>
+          <li className={styles.listItem}>Menu</li>
+          <Image src="/img/logo.png" alt="" width="160" height="69" />
+          <li className={styles.listItem}>Events</li>
+          <li className={styles.listItem}>Blog</li>
+          <li className={styles.listItem}>Contact</li>
+        </ul>
+      </div>
+
+      <div className={styles.item}>
+        <div className={styles.cart}>
+          <Image src="/img/cart.png" alt="" width="30" height="30" />
+          <div className={styles.counter}>2</div>
+        </div>
+      </div>
+      
     </div>
   );
 };
